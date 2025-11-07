dc4835e81782c8a20b92afc41da9bd4d7bfb2ce0
Miracle <Miraclecampchat@icloud.com>
2025-11-07 09:01:26 +0500

    add some files

--- UdemyJS/Modal/starter/script.js
+++ UdemyJS/Modal/starter/script.js
@@ -4,12 +4,25 @@ const modal = document.querySelector('.modal');
 
 const overlay = document.querySelector('.overlay');
 
-const btnCloseModal = document.querySelector('close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const body = document.querySelector('.body');
 
-for (let i = 0; i <= btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
-}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
